74ff0703cae2522f53dbf424eb3c6b517062715c
Tomas Ruiz <truizsanchez@gmail.com>
2020-12-26 10:08:50 +0100

    increased difficulty and levels added

--- game/entities/enemy.py
+++ game/entities/enemy.py
@@ -2,7 +2,7 @@ from engine.entities.base import BaseEntity
 
 
 class Enemy(BaseEntity):
-    VX_DEFAULT = 0.4
-    VY_DEFAULT = 0.4
+    VX_DEFAULT = 0.6
+    VY_DEFAULT = 0.6
     WIDTH = 8
     HEIGHT = 8
